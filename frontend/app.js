1845dbf452cabfd4d4b782fa2ae8c7dc75c02cec
taskmanager461 <workmail4416@gmail.com>
2026-05-08 22:08:49 +0300

    Add files via upload

--- frontend/app.js
+++ frontend/app.js
@@ -1018,7 +1018,7 @@ function renderHeroMetrics(score) {
             </div>
             <div class="hero-metric-label">Self Trust Score</div>
             <div class="hero-metric-value">${scoreVal}</div>
-            <div class="status-badge-container" style="background-image: url('${img3}'); background-position: ${statusPos}; background-color: transparent !important; width: 120px; height: 55px;"></div>
+            <div class="status-badge-container" style="background-image: url('${img3}'); background-position: ${statusPos}; background-color: transparent !important; width: 140px; height: 86px;"></div>
         </div>
 
         <!-- Card 2: Streak -->
