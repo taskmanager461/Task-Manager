0ab191cd06843d19caae5cb4095c92112bd0f98f
taskmanager461 <workmail4416@gmail.com>
2026-05-21 19:07:24 +0300

    Add files via upload

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "task-manager-v6.2.5";
+const CACHE_NAME = "task-manager-v6.2.6";
 const ASSETS_TO_CACHE = [
   "/",
   "/index.html",
-  "/styles.css?v=6.2.5",
-  "/app.js?v=6.2.5",
-  "/assets.js?v=6.2.5",
+  "/styles.css?v=6.2.6",
+  "/app.js?v=6.2.6",
+  "/assets.js?v=6.2.6",
   "/manifest.json",
   "/static/icon-home-192-v7.png",
   "/static/icon-home-512-v7.png"
