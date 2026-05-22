3438e8f4256aa4f9e9a04a48978f1716864282ba
taskmanager461 <workmail4416@gmail.com>
2026-05-22 23:11:34 +0300

    Add files via upload

--- frontend/app.js
+++ frontend/app.js
@@ -1793,7 +1793,7 @@ function getBadgeImageSrc(scoreClass) {
         low: 'badge_low.png',
     };
     const name = map[scoreClass] || map.low;
-    return `/static/${name}?v=1`;
+    return `/static/${name}?v=2`;
 }
 
 // --- Reports & Me Logic ---
